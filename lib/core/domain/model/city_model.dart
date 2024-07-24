9d4b474330e6fa5e2d192b1248d0bf6586277d2f
yysrll <yusril.uzumaki@gmail.com>
2024-07-24 11:02:09 +0700

    build: add equatble and implement to model class

--- lib/core/domain/model/city_model.dart
+++ lib/core/domain/model/city_model.dart
@@ -1,9 +1,11 @@
-class CityModel {
+import 'package:equatable/equatable.dart';
+
+class CityModel extends Equatable {
+  const CityModel({required this.id, required this.name});
+
   final String id;
   final String name;
 
-  CityModel({
-    required this.id,
-    required this.name,
-  });
+  @override
+  List<Object?> get props => [id, name];
 }
